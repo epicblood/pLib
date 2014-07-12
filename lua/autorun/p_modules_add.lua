7a8e8b9542537e6f8dcc37323d4ed226dc101d37
thelastpenguin <thelastpenguin212@gmail.com>
2014-07-12 00:00:09 -0700

    feat(rpc): Implemented RPC Lib

--- lua/autorun/p_modules_add.lua
+++ lua/autorun/p_modules_add.lua
@@ -3,4 +3,5 @@ if SERVER then
 	AddCSLuaFile( 'includes/modules/pon.lua' );
 	AddCSLuaFile( 'includes/modules/xfn.lua' );
 	AddCSLuaFile( 'includes/modules/async.lua' );
+	AddCSLuaFile( 'includes/modules/rpc.lua' );
 end
